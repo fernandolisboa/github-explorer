0f4c74baef73c227528a7f865c99aa45f39d0d72
Fernando Lisboa <fernandoigorlisboa@gmail.com>
2020-07-06 17:09:18 -0300

    styled dashboard and added axios (get github api)

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,98 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+
+  margin-top: 100px;
+  max-width: 435px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 715px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 72px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 72px;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: white;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 715px;
+
+  a {
+    text-decoration: none;
+    background: #fff;
+    border-radius: 5px;
+    padding: 16px;
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      height: 80px;
+      width: 80px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 24px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
